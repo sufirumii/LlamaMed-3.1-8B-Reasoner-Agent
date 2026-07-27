a8a5676cc4b63655f5323db16a3e2ebbcc156e7c
Rumi Iqbal Sufi <sufirumii@gmail.com>
2026-07-27 12:44:03 +0530

    Create pip install.py

---
+++ pip install.py
@@ -0,0 +1,3 @@
+!pip install --no-deps bitsandbytes accelerate xformers==0.0.29.post3 peft trl==0.15.2 triton cut_cross_entropy unsloth_zoo
+!pip install sentencepiece protobuf "datasets>=3.4.1" huggingface_hub hf_transfer
+!pip install --no-deps unsloth
